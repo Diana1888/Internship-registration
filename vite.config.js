0437c6489e219756b31a054e99d49e9f02c1ce65
Diana Ichmoukhametov <akhtyamovadiana@gmail.com>
2025-08-16 23:29:20 -0400

    Added tab icon

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Internship-registration/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // <-- добавляем алиас "@"
@@ -13,4 +14,4 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
   },
-})
+});
